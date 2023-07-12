8cf982be52f41220149f2a305b7e94530f25aeea
voste126 <steveaustine126@gmail.com>
2023-07-12 13:22:40 +0300

    my first OPP

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your code here
+//object oriented programming
+
+class Breakfast{
+    constructor(food,drink){
+        this.food = food;
+        this.drink = drink;
+    }
+}
+
+class Lunch{
+    constructor(salad,soup,drink){
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+}
+
+class Dinner{
+    //private fields 
+    #dessert;
+    constructor(salad,soup,entree,dessert){
+        this.#dessert = dessert;
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+    }
+}
